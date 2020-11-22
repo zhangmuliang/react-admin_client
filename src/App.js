bfa81bc5e7fe976364264c11a2100adfe26a1eb9
zhangmuliang <714978916@qq.com>
2020-11-22 14:52:40 +0800

    引入antd以及按需打包

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 // 应用的根组件
 import React, { Component } from "react";
+import { message, Button } from 'antd';
 
 export default class App extends Component {
-    render(){
-        return <div>App</div>
+    handleClick = () => {
+        message.success('成功啦');
+    }
+    render() {
+        return <Button type="primary" onClick={this.handleClick}>测试antd</Button>;
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 // 入口js
 import React, { Component } from "react";
-import ReactDOM from "react-dom"
+import ReactDOM from "react-dom";
+// import "antd/dist/antd.css";
 
-import App from "./App"
+import App from "./App";
 
 // 将App组件标签渲染到index页面的div上
-ReactDOM.render(<App/>, document.getElementById('root'))
+ReactDOM.render(<App/>, document.getElementById('root'));
