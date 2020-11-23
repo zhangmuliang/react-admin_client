5d9aabe60b6b3ddcb6f0ba87b52d984924aade5e
zhangmuliang <714978916@qq.com>
2020-11-23 10:29:18 +0800

    完成部分左侧导航路由配置

--- src/components/left-nav/left-nav.jsx
+++ src/components/left-nav/left-nav.jsx
@@ -1,12 +1,65 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom"
+import { Menu } from 'antd';
+import {
+    AppstoreOutlined,
+    PieChartOutlined,
+} from '@ant-design/icons';
 
+import logo from "../../assets/images/logo.png"
 import './left-nav.less'
 
-export default class LeftNav extends Component{
+const { SubMenu } = Menu;
 
-    render () {
+export default class LeftNav extends Component {
+
+    render() {
         return (
-            <div className="left-nav">LeftNav</div>
+            <div className="left-nav">
+                <Link to='/home' className="left-nav-header">
+                    <img src={logo} alt="logo" />
+                    <h1>小张后台</h1>
+                </Link>
+
+                <Menu
+                    defaultSelectedKeys={['/home']}
+                    defaultOpenKeys={['sub1']}
+                    mode="inline"
+                    theme="dark"
+                >
+                    <Menu.Item key="/home" icon={<PieChartOutlined />}>
+                        <Link to='/home'>
+                            <span>首页</span>
+                        </Link>
+                    </Menu.Item>
+
+                    <SubMenu key="sub1" icon={<AppstoreOutlined />} title="商品">
+                        <Menu.Item key="/category" icon={<AppstoreOutlined />}>
+                            <Link to='/category'>
+                                <span>品类管理</span>
+                            </Link>
+                        </Menu.Item>
+                        <Menu.Item key="/product" icon={<AppstoreOutlined />}>
+                            <Link to='/product'>
+                                <span>商品管理</span>
+                            </Link>
+                        </Menu.Item>
+                    </SubMenu>
+
+                    <Menu.Item key="/user" icon={<PieChartOutlined />}>
+                        <Link to='/user'>
+                            <span>用户管理</span>
+                        </Link>
+                    </Menu.Item>
+
+                    <Menu.Item key="/role" icon={<PieChartOutlined />}>
+                        <Link to='/role'>
+                            <span>角色管理</span>
+                        </Link>
+                    </Menu.Item>
+                </Menu>
+            </div>
+
         )
     }
 }
--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
-import { Redirect } from "react-router-dom";
+import { Redirect, Switch, Route } from "react-router-dom";
 import { Layout } from 'antd';
 
 import memoryUtils from "../../utils/memoryUtils";
 import LeftNav from "../../components/left-nav/left-nav";
 import HeaderNav from "../../components/header/header-nav";
+import Home from "../home/home";
+import Category from "../category/category";
+import Product from "../product/product";
+import Role from "../role/role";
+import User from "../user/user";
+import Bar from "../charts/bar";
+import Line from "../charts/line";
+import Pie from "../charts/pie";
 
 const { Footer, Sider, Content } = Layout;
 
@@ -24,8 +32,20 @@ export default class Admin extends Component {
                 </Sider>
                 <Layout>
                     <HeaderNav />
-                    <Content style={{ backgroundColor: "black" }}>Content</Content>
-                    <Footer style={{ textAlign: "center", color: "#ccc" }}>推荐使用火狐浏览器，可以获得最佳页面体验效果</Footer>
+                    <Content>
+                        <Switch>
+                            <Route path='/home' component={Home} />
+                            <Route path='/category' component={Category} />
+                            <Route path='/product' component={Product} />
+                            <Route path='/role' component={Role} />
+                            <Route path='/user' component={User} />
+                            <Route path='/charts/bar' component={Bar} />
+                            <Route path='/charts/line' component={Line} />
+                            <Route path='/charts/pie' component={Pie} />
+                            <Redirect to='/home' />
+                        </Switch>
+                    </Content>
+                    <Footer style={{ textAlign: "center", borderTop:"1px solid gray", color: "#ccc" }}>推荐使用火狐浏览器，可以获得最佳页面体验效果</Footer>
                 </Layout>
             </Layout>
         )
